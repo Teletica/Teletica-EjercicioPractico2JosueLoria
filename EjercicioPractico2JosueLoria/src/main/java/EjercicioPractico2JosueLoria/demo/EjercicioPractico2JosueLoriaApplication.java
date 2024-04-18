744a41eb18caafbc5616ff4ef06492ca9cc8f28e
Josue Loria <jmiranda90400@ufide.ac.cr>
2024-04-17 18:09:02 -0600

    Creacion del proyecto
    Nada mas cree el proyecto y modifique el pom

---
+++ EjercicioPractico2JosueLoria/src/main/java/EjercicioPractico2JosueLoria/demo/EjercicioPractico2JosueLoriaApplication.java
@@ -0,0 +1,13 @@
+package EjercicioPractico2JosueLoria.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPractico2JosueLoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPractico2JosueLoriaApplication.class, args);
+	}
+
+}
---
+++ EjercicioPractico2JosueLoria/src/test/java/EjercicioPractico2JosueLoria/demo/EjercicioPractico2JosueLoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package EjercicioPractico2JosueLoria.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPractico2JosueLoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
